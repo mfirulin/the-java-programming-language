624dbc478dd3bf6c40837de4a0b849393f88f4d3
Mikhail Firulin <mfirulin@gmail.com>
2016-09-27 14:03:59 +0300

    Create MyUtilities.java

---
+++ 01/15/MyUtilities.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.io.FileInputStream;
+
+class BadDataSetException extends Exception {
+    public String filename;
+    public IOException exception;
+    
+    public BadDataSetException(String name, IOException e) {
+        filename = name;
+        exception = e;
+    }
+}
+
+public class MyUtilities {
+
+    public double[] getDataSet(String setName) 
+        throws BadDataSetException
+    {
+        String file = setName + ".dset";
+        FileInputStream in = null;
+        
+        try {
+            in = new FileInputStream(file);
+            return readDataSet(in);
+        } catch (IOException e) {
+            throw new BadDataSetException(file, e);
+        } finally {
+            try {
+                if (in != null) in.close();
+            } catch (IOException e) {
+                ; // No action
+            }
+        }
+    }
+    
+    private double[] readDataSet(FileInputStream in) {
+        return new double[0];
+    }
+    
+    public static void main(String []args){
+        MyUtilities mu = new MyUtilities();
+        try {
+            mu.getDataSet("some_name");
+        } catch (BadDataSetException e) {
+            System.out.println(e.filename);
+            System.out.println(e.exception);
+        }
+    }
+}
