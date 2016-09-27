6a744198628cd11123e14c97102ed689b9954b0c
Mikhail Firulin <mfirulin@gmail.com>
2016-09-27 15:56:16 +0300

    Update LinkedList.java

--- 02/06/LinkedList.java
+++ 02/06/LinkedList.java
@@ -1,22 +1,44 @@
 public class LinkedList {
     public Object obj;
     private LinkedList next;
-    
+
     public static void main(String[] args) {
-        int i;
-        final int MAX = 5;
         LinkedList list = new LinkedList();
-        LinkedList l = list;
-        
-        for(i = 0; i < MAX; ++i) {
-            Vehicle v = new Vehicle();
-            l.obj = v;
-            l.next = new LinkedList();
-            l = l.next;
+        LinkedList head = list;
+
+        Vehicle v = new Vehicle();
+        v.speed = 60;
+        v.course = 45;
+        v.owner = "John";
+        v.number = Vehicle.nextFreeNumber++;
+        list.obj = v;
+
+        list.next = new LinkedList();
+        list = list.next;
+
+        v = new Vehicle();
+        v.speed = 62;
+        v.course = 43;
+        v.owner = "Helen";
+        v.number = Vehicle.nextFreeNumber++;
+        list.obj = v;
+
+        list.next = new LinkedList();
+        list = list.next;
+
+        v = new Vehicle();
+        v.speed = 59;
+        v.course = 42;
+        v.owner = "Mark";
+        v.number = Vehicle.nextFreeNumber++;
+        list.obj = v;
+
+        for(list = head; list != null; list = list.next) {
+            v = (Vehicle) list.obj;
+            System.out.println("Vehicle: number " + v.number + 
+                               ", owner " + v.owner +
+                               ", speed " + v.speed + 
+                               ", course " + v.course);
         }
-        
-        for(i = 0, l = list; l != null; l = l.next)
-            ++i;
-        System.out.println(i);
     }
 }
