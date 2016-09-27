a5a893281b0c2639ab2774d6836a5a811720a6bd
Mikhail Firulin <mfirulin@gmail.com>
2016-09-27 15:38:06 +0300

    Update Vehicle.java

--- 02/05/Vehicle.java
+++ 02/05/Vehicle.java
@@ -1,15 +1,24 @@
 public class Vehicle {
-    private double speed;
-    private short course;
-    private String owner;
-    private long number;
-    
+    public double speed;
+    public short course;
+    public String owner;
+    public long number;
+
     public static long nextFreeNumber;
-    
+
     public static void main(String[] args) {
         Vehicle bmv = new Vehicle();
+        bmv.speed = 60;
+        bmv.course = 45;
+        bmv.owner = "John";
+        bmv.number = nextFreeNumber++;
+
         Vehicle lada = new Vehicle();
-        
+        lada.speed = 50;
+        lada.course = 34;
+        lada.owner = "Helen";
+        lada.number = nextFreeNumber++;  
+
         System.out.println("Vehicle 1: number " + bmv.number + 
                            ", owner " + bmv.owner +
                            ", speed " + bmv.speed + 
