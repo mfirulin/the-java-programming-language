3d0d54eb04a5be0953a2add8f882042bd5c175a2
Mikhail Firulin <mfirulin@gmail.com>
2016-09-27 10:22:15 +0300

    Create ImprovedFibonacci.java

---
+++ 01/1.7/ImprovedFibonacci.java
@@ -0,0 +1,21 @@
+public class ImprovedFibonacci {
+    static final int MAX = 9;
+
+    public static void main(String[] args) {
+        int lo = 1;
+        int hi = 1;
+        String mark;
+
+        System.out.println(MAX + ": " + lo);
+        for(int i = MAX - 1; i > 0; --i) {
+            if(hi % 2 == 0)
+                mark = " *";
+            else
+            mark = "";
+
+            System.out.println(i + ": " + hi + mark);
+            hi += lo;
+            lo = hi - lo;
+        }
+    }
+}
