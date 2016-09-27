a821344a74ccfa2bcb732c9334fc9b2d88fa68a6
Mikhail Firulin <mfirulin@gmail.com>
2016-09-27 10:17:27 +0300

    Update and rename ch01/1.5/HelloWorld.java to 01/1.5/HelloWorld.java

--- 01/1.5/HelloWorld.java
+++ 01/1.5/HelloWorld.java
@@ -1,7 +1,7 @@
-class HelloWorld {
+public class HelloWorld {
     static final String str = "Hello, World!";
     
     public static void main(String[] args) {
-	    System.out.println(str);
+        System.out.println(str);
     }
 }
