a34cb9ae987e1828d38ca9c52caf673bdd22ffe8
Mikhail Firulin <mfirulin@gmail.com>
2016-09-27 09:57:22 +0300

    Create Fibonacci.java

---
+++ ch01/1.3/Fibonacci.java
@@ -0,0 +1,15 @@
+class Fibonacci {
+    public static void main(String[] args) {
+	    long lo = 1;
+	    long hi = 1;
+
+	    System.out.println("fibonacci");
+	    System.out.println("---------");
+	    System.out.println(lo);
+	    while(hi < 50) {
+	        System.out.println(hi);
+	        hi += lo;
+	        lo = hi - lo;
+	    }   
+    }
+}
