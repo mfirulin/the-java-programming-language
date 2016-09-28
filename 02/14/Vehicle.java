c374c6649ba1692171b198f04a65fb8ce1cacdcd
Mikhail Firulin <mfirulin@gmail.com>
2016-09-28 16:21:45 +0300

    Create Vehicle.java

---
+++ 02/14/Vehicle.java
@@ -0,0 +1,96 @@
+public class Vehicle
+{
+    private double speed;
+    private short course;
+    private String owner = "<ownerless>";
+    private long number;
+    
+    private static long nextFreeNumber = 1;
+
+    // Constructors    
+    Vehicle()
+    {
+        number = nextFreeNumber++;
+    }
+    
+    Vehicle(String o)
+    {
+        this();
+        owner = o;
+    }
+
+    public void stop()
+    {
+        speed = 0;
+    }
+
+    // Get, Set
+    public double getSpeed()
+    {
+        return speed;
+    }
+    
+    public void setSpeed(double s)
+    {
+        speed = s;
+    }
+    
+    public short getCourse()
+    {
+        return course;
+    }
+    
+    public void setCourse(short c)
+    {
+        course = c;
+    }
+
+    public String getOwner()
+    {
+        return owner;
+    }
+    
+    public void setOwner(String o)
+    {
+        owner = o;
+    }
+
+    public long getNumber()
+    {
+        return number;
+    }
+
+  	public void changeSpeed(double s) {
+        speed += s;
+    }
+  
+    public String toString()
+    {
+        return getClass() + "[number=" + number + 
+            ",owner=" + owner +
+            ",speed=" + speed + 
+            ",course=" + course +
+            "]";
+    }
+    
+    // Static methods
+    public static long getNextFreeNumber()
+    {
+        return nextFreeNumber;
+    }
+
+    public static long getMaxNumber()
+    {
+        return nextFreeNumber - 1;
+    }
+    
+    public static void main(String[] args)
+    {
+        Vehicle bmv = new Vehicle("Mikhail");
+        
+        System.out.println(bmv);
+        bmv.setSpeed(50);
+        bmv.changeSpeed(5);
+        System.out.println(bmv);
+    }
+}
