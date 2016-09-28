c13fa3ee82e1a1cbd10d833751358dc431963ad1
Mikhail Firulin <mfirulin@gmail.com>
2016-09-28 16:33:14 +0300

    Create LinkedList.java

---
+++ 02/15/LinkedList.java
@@ -0,0 +1,35 @@
+public class LinkedList {
+    private Object obj;
+    private LinkedList next;
+
+    public Object getObject() {
+        return obj;
+    }
+    
+    public void setObject(Object o) {
+        obj = o;
+    }
+
+    public String toString() {
+ 		return getClass() + "[object=" + obj + "]";
+    }
+
+    public int getNumberOfElements() {
+        int i = 0;
+        for(LinkedList list = this; list != null; list = list.next)
+            i++;
+        return i;
+    }
+    
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        LinkedList node = list;
+        
+        for(int i = 0; i < 5; i++) {
+            node.next = new LinkedList();
+            node = node.next;
+        }
+        
+        System.out.println("Number of elements in the list: " + list.getNumberOfElements());
+    }
+}
