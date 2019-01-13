099a088eb692aa4fa3952852ef4717a101a67acd
Mikhail Firulin <mfirulin@gmail.com>
2019-01-13 12:44:33 +0300

    Create TickTack.java

---
+++ chapter10/TickTack.java
@@ -0,0 +1,28 @@
+public class TickTack implements Runnable {
+    @Override
+    public void run() {
+        try {
+            for (;;) {
+                System.out.print(".");
+                System.out.flush();
+                Thread.sleep(1000);
+            }
+        }
+        catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    
+    public static void main(String[] args) {
+        var thread = new Thread(new TickTack());
+        thread.start();
+        
+        try {
+            Thread.sleep(10000);
+        }
+        catch (InterruptedException e) {
+        }
+        
+        thread.interrupt();
+    }
+}
