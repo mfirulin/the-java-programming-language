db78db319be8eb24082a7761351a004540d0fc04
Mikhail Firulin <mfirulin@gmail.com>
2020-01-15 11:20:12 +0300

    Update Vehicle.java

--- chapter02/exercise03/Vehicle.java
+++ chapter02/exercise03/Vehicle.java
@@ -1,3 +1,6 @@
+// Exercise 2.3: Add a static field to your Vehicle class to hold the next vehicle identification number, 
+// and a non-static field to the Vehicle class to hold each car's ID number.
+
 public class Vehicle {
     public double speed;
     public short course;
