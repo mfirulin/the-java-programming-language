1c6484089cb9aecca618f8fe833132510e14d772
Mikhail Firulin <mfirulin@gmail.com>
2020-01-15 11:17:41 +0300

    Update Vehicle.java

--- chapter02/exercise01/Vehicle.java
+++ chapter02/exercise01/Vehicle.java
@@ -1,3 +1,6 @@
+// Exercise 2.1: Write a simple Vehicle class that has fields for (at least) current speed, 
+// current direction in degrees, and owner name.
+
 public class Vehicle {
     public double speed;
     public short course;
