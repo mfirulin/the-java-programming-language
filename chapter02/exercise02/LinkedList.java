ef1495dd468cf508fd374f037a6ac369bb533b74
Mikhail Firulin <mfirulin@gmail.com>
2020-01-15 11:18:30 +0300

    Update LinkedList.java

--- chapter02/exercise02/LinkedList.java
+++ chapter02/exercise02/LinkedList.java
@@ -1,3 +1,6 @@
+// Exercise 2.2: Write a LinkedList class that has a field of type Object and 
+// a reference to the next LinkedList element in the list.
+
 public class LinkedList {
     public Object obj;
     private LinkedList next;
