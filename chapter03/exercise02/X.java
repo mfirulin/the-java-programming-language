f3d7212242f5dbf4150a7a34b06ad11fa770a50c
Mikhail Firulin <mfirulin@gmail.com>
2020-01-17 18:27:17 +0300

    Update X.java

--- chapter03/exercise02/X.java
+++ chapter03/exercise02/X.java
@@ -1,3 +1,7 @@
+// Exercise 3.2: Type in the classes X and Y as shown previously, and add print statements to trace the
+// values of the masks. Add a main method and run it to see the results. (Hint: Use the printf
+// methodshown in Chapter 1with a format specifier of %x to print integers in hexadecimal format.)
+
 class X {
     protected int xMask = 0x00ff;
     protected int fullMask;
