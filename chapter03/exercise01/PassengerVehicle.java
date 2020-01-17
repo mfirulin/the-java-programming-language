003f4123874a054c9bda1f8e439066e6b92d71f8
Mikhail Firulin <mfirulin@gmail.com>
2020-01-17 18:26:15 +0300

    Update PassengerVehicle.java

--- chapter03/exercise01/PassengerVehicle.java
+++ chapter03/exercise01/PassengerVehicle.java
@@ -1,3 +1,8 @@
+// Exercise 3.1: Starting with the Vehicle class from the exercises in Chapter 2, create an extended class
+// called PassengerVehicle to add a capability for counting the number of seats available in the car and the
+// number currently occupied. Provide a new main method in PassengerVehicle to create a few of these
+// objects and print them out.
+
 public class PassengerVehicle extends Vehicle {
     private int numOfPlaces;
     private int numOfFreePlaces;
